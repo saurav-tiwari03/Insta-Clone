87ab891637c0478f8f5b0e47ed6c82bc32922a94
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-06-01 03:37:29 +0530

    authStatus

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route, Navigate } from 'react-router-dom'
 import {Home} from './pages/Home/Home'
 import {Login} from './pages/Auth/Login'
 import {Signup} from './pages/Auth/Signup'
 
 export default function App() {
+  const authStatus = false
   return (
     <>
       <div className=''>
         <Routes>
-          <Route path='/' element={<Home />}/>
+          <Route path='/' element={authStatus ? <Home /> : <Navigate to='/login' />}/>
           <Route path='/login' element={<Login />}/>
           <Route path='/signup' element={<Signup />}/>
         </Routes>
